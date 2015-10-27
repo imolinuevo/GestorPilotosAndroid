bc917f694088ea4296409305f422c61bcf845d04
Inigo Molinuevo <i.m.parron@opendeusto.es>
2015-10-27 11:07:55 +0100

    Listado layout fiished. Iñigo Molinuevo

--- app/src/main/java/miw/dasm/inigo/gestorpilotos/PilotoAdapter.java
+++ app/src/main/java/miw/dasm/inigo/gestorpilotos/PilotoAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.List;
@@ -23,6 +24,12 @@ public class PilotoAdapter extends ArrayAdapter<Piloto> {
         Piloto piloto = getItem(position);
         TextView nombrePiloto = (TextView) convertView.findViewById(R.id.nombrePiloto);
         nombrePiloto.setText(piloto.get_nombre());
+        TextView dorsalPiloto = (TextView) convertView.findViewById(R.id.dorsalPiloto);
+        dorsalPiloto.setText(new Integer(piloto.get_dorsal()).toString());
+        TextView motoPiloto = (TextView) convertView.findViewById(R.id.motoPiloto);
+        motoPiloto.setText(piloto.get_moto());
+        CheckBox activoPiloto = (CheckBox) convertView.findViewById(R.id.activoPiloto);
+        activoPiloto.setChecked(piloto.is_activo());
         return convertView;
     }
 
