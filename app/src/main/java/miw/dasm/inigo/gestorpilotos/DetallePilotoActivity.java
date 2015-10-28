394ee817378f78f9ae9d703a14db4d30c63455de
Inigo Molinuevo <i.m.parron@opendeusto.es>
2015-10-28 10:50:22 +0100

    Dusting imports. Iñigo Molinuevo

--- app/src/main/java/miw/dasm/inigo/gestorpilotos/DetallePilotoActivity.java
+++ app/src/main/java/miw/dasm/inigo/gestorpilotos/DetallePilotoActivity.java
@@ -1,12 +1,8 @@
 package miw.dasm.inigo.gestorpilotos;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
